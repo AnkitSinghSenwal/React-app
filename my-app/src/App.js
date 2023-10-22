2f9b6a4844015ddc4d07ad50dd162775395422e8
AnkitSinghSenwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2023-10-22 18:36:26 +0000

    just working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+let name = "React app"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
+        {name}
+        <Demo></Demo>
+      </header>
+    </div>
+  );
+}
+
+function Demo() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Demo 
       </header>
     </div>
   );
