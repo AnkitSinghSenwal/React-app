ef80c616a0ed18df0df530b2317832971aca26c0
AnkitSinghSenwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2023-10-23 12:08:17 +0000

    adding image tag to video file

--- my-app/src/component/Video.js
+++ my-app/src/component/Video.js
@@ -1,7 +1,6 @@
 function Video(){
-    return <div> I am from video</div>
+
+    return (<div> <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'></img><div> I am from video</div></div>)
 }
-function Thumb(){
-    return <div> This is a thumd form Thumb function</div>
-}
-export  {Video, Thumb};
+
+export  default Video;
