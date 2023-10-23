87d22ccbc38325c56bf1ceba6774e8b0e8aefbc7
AnkitSinghSenwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2023-10-23 12:29:55 +0000

    creating a css file for Video

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
 import Video from "./component/Video";
 
 function App(){
-  return (<div><div>Hello from App.js file</div> <Video></Video></div>
+  return (<>
+  <div>Hello from App.js file</div> 
+  <Video></Video>
+  </>
   )
 }
 
--- my-app/src/component/Video.js
+++ my-app/src/component/Video.js
@@ -1,8 +1,11 @@
-function Video(){
+import './Video.css';
 
+function Video(){
+    let topic = "React Js";
+    let bg = 'dark';
     return (<> 
     <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'></img>
-    <div> I am from video</div>
+    <div className={bg}> I am from video</div>
     </>)
 }
 
