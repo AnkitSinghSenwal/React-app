8afb1abdc410ab2e49e41eec62d62e7058b75874
AnkitSinghSenwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2023-10-23 12:16:47 +0000

    using fragment in return

--- my-app/src/component/Video.js
+++ my-app/src/component/Video.js
@@ -1,6 +1,9 @@
 function Video(){
 
-    return (<div> <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'></img><div> I am from video</div></div>)
+    return (<> 
+    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'></img>
+    <div> I am from video</div>
+    </>)
 }
 
 export  default Video;
