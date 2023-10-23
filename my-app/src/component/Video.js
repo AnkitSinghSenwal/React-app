20848225ee26ddec2bc1b7cd1bb1672dccf36c82
AnkitSinghSenwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2023-10-23 12:33:06 +0000

    removing a closing tag

--- my-app/src/component/Video.js
+++ my-app/src/component/Video.js
@@ -4,7 +4,7 @@ function Video(){
     let topic = "React Js";
     let bg = 'dark';
     return (<> 
-    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'></img>
+    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'/>
     <div className={bg}> I am from video</div>
     </>)
 }
