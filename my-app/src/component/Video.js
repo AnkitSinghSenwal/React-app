12bbd6fbacf2e3c6bc2766557bae33ce5444ea09
AnkitSinghSenwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2023-10-25 15:46:35 +0000

    adding css

--- my-app/src/component/Video.js
+++ my-app/src/component/Video.js
@@ -4,13 +4,15 @@ function Video({title, channel, views, time}){
     
     let bg = 'dark';
     
-    return (<> 
+    return (
+    <>
+    <div className = "container">
     <div className="pic"><img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" width='160' height='90'/></div>
     <div className = "title"><div className={bg} > {title}</div>
     </div>
     <div className = "channel">{channel}</div>
     <div className = "views">{views}<span>.</span> {time}</div>
-    <div className = ""></div>
+    </div>
 
     
     </>)
